e0f8a8fb095553ff45a4b6f6e786a00e1e8cadcd
nowhere <32817067+jeaimetu@users.noreply.github.com>
2020-04-11 23:33:38 +0900

    Update web.js
    delete contract.js because this is not needed

--- web.js
+++ web.js
@@ -3,7 +3,7 @@ var bodyParser     =        require("body-parser");
 var app = express();
 var path = require('path');
 
-const contract = require("./contract");
+
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
